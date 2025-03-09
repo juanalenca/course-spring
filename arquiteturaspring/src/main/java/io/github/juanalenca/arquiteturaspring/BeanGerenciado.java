c82c929259fbfc85350955ec0e40cb932f95be1b
juanalenca <juanbarros.dev@gmail.com>
2025-03-09 15:54:23 -0300

    Escopo dos beans @Scope

--- arquiteturaspring/src/main/java/io/github/juanalenca/arquiteturaspring/BeanGerenciado.java
+++ arquiteturaspring/src/main/java/io/github/juanalenca/arquiteturaspring/BeanGerenciado.java
@@ -3,9 +3,22 @@ package io.github.juanalenca.arquiteturaspring;
 import io.github.juanalenca.arquiteturaspring.todos.TodoEntity;
 import io.github.juanalenca.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/*
+ * Escopo Singleton - Padrão do Spring:
+ * O padrão Singleton garante que apenas uma única instância do objeto seja criada e compartilhada
+ * por toda a aplicação. Esse padrão é útil quando se deseja manter um estado global ou evitar
+ * a criação desnecessária de múltiplas instâncias do mesmo objeto.
+ *
+ * Outros escopos disponíveis no Spring:
+ * - request: Criado e utilizado apenas durante o ciclo de vida de uma única requisição HTTP.
+ * - session: Atrelado à sessão do usuário, permanecendo ativo enquanto a sessão existir.
+ * - application: Compartilhado em toda a aplicação, similar ao singleton, mas gerenciado de forma diferente.
+ */
 @Component
+@Scope("singleton")
 public class BeanGerenciado {
 
     //injeção via propriedada sem precisar de um construtor
