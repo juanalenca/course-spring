10e88043c2b2c755e68c9f9c3afccd04fa4eca3e
juanalenca <juanbarros.dev@gmail.com>
2025-03-11 13:41:29 -0300

    Mapeamento da entidade Livro e utilização do Lombok

---
+++ libraryapi/src/main/java/io/github/juanalenca/libraryapi/model/GeneroLivro.java
@@ -0,0 +1,16 @@
+package io.github.juanalenca.libraryapi.model;
+
+public enum GeneroLivro {
+    FICCAO,
+    NAO_FICCAO,
+    ROMANCE,
+    FANTASIA,
+    TERROR,
+    SUSPENSE,
+    AVENTURA,
+    CIENCIA_FICCAO,
+    BIOGRAFIA,
+    HISTORIA,
+    POESIA,
+    DRAMA
+}
